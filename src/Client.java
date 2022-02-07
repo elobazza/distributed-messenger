a96b86b48ae836c0de7b93b0207c91b5c6a36bae
Eloísa Bazzanella <elobazzanella@gmail.com>
2022-02-07 13:40:53 -0300

    Added Documents

--- src/Client.java
+++ src/Client.java
@@ -1,81 +1,40 @@
-
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
 
 /**
  * @author Eloisa e Maria Eduarda
  */
-public class Client {
+public class Client extends Thread {
  
+//    private Socket socket;
+    
     private String host;
     private int porta;
-    private Socket socket;
-    DataOutputStream output;
-    private Scanner input;
+    
+    private String hostContato;
+    private int portaContato;
+    
+    private boolean running;
 
-    public Client(String host, int porta) {
-        this.host = host;
-        this.porta = porta;
-        this.input = new Scanner(System.in);
-        
+    public Client(String hostContato, int portaContato) {
+        this.hostContato  = hostContato;
+        this.portaContato = portaContato;
+    }
+    
+    public void run() {
+        System.out.println("Porta recebida do contato: " + portaContato);
+        connect();
+    }
+    
+    public void connect() {
         try {
-            this.socket = new Socket(host, porta);
-            System.out.println("Conectado");
+            Socket socket = new Socket(hostContato, portaContato);
             
-            this.output = new DataOutputStream(this.socket.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            while (running) {
+                
+            }
             
-//            this.operacoes();            
-            
-        }  catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPorta() {
-        return porta;
-    }
-
-    public void setPorta(int porta) {
-        this.porta = porta;
-    }
-
-    public Socket getSocket() {
-        return socket;
-    }
-
-    public void setSocket(Socket socket) {
-        this.socket = socket;
-    }
-
-    public DataOutputStream getOutput() {
-        return output;
-    }
-
-    public void setOutput(DataOutputStream output) {
-        this.output = output;
-    }
-
-    public Scanner getInput() {
-        return input;
-    }
-
-    public void setInput(Scanner input) {
-        this.input = input;
-    }
-    
-    
 }
