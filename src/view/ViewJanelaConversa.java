4db2b8ae785e1dc0f77056af835a5526a26ce2c6
Eloísa Bazzanella <elobazzanella@gmail.com>
2022-02-07 20:22:38 -0300

    Lista Mensagens

--- src/view/ViewJanelaConversa.java
+++ src/view/ViewJanelaConversa.java
@@ -2,6 +2,7 @@ package view;
 
 import controller.ControllerConversa;
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  *
@@ -10,7 +11,9 @@ import java.awt.Color;
 public class ViewJanelaConversa extends javax.swing.JFrame {
     
     private ControllerConversa controller;
-
+    
+    private String[] mensagens;
+    
     public ViewJanelaConversa(String nome) {
         initComponents();
         this.getContentPane().setBackground(new Color(242, 236, 228));
@@ -27,11 +30,18 @@ public class ViewJanelaConversa extends javax.swing.JFrame {
         tfMensagem = new javax.swing.JTextField();
         pnChat = new javax.swing.JPanel();
         lbNome = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        chat = new javax.swing.JList<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         btEnviar.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
         btEnviar.setText("Enviar");
+        btEnviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btEnviarActionPerformed(evt);
+            }
+        });
 
         lbNome.setFont(new java.awt.Font("Calibri", 1, 24)); // NOI18N
         lbNome.setText("Nome Pessoa");
@@ -53,23 +63,36 @@ public class ViewJanelaConversa extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        chat.setModel(new javax.swing.AbstractListModel<String>() {
+            String[] strings = {};
+            public int getSize() { return strings.length; }
+            public String getElementAt(int i) { return strings[i]; }
+        });
+        chat.setBackground(new Color(242, 236, 228));
+        jScrollPane1.setViewportView(chat);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(pnChat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addGap(18, 18, 18)
-                .addComponent(tfMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btEnviar, javax.swing.GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
-                .addContainerGap())
-            .addComponent(pnChat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 417, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(tfMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btEnviar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(pnChat, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 250, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 238, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(btEnviar, javax.swing.GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
                     .addComponent(tfMensagem))
@@ -80,8 +103,17 @@ public class ViewJanelaConversa extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btEnviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btEnviarActionPerformed
+        String novaMensagem = tfMensagem.getText();
+        
+                
+        chat.setListData(mensagens);
+    }//GEN-LAST:event_btEnviarActionPerformed
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btEnviar;
+    private javax.swing.JList<String> chat;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lbNome;
     private javax.swing.JPanel pnChat;
     private javax.swing.JTextField tfMensagem;
